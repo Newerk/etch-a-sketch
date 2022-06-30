95b0124a0ebaebf7894057f1edef3818519055a2
newerk <juniusowens@yahoo.com>
2022-06-30 17:05:22 -0400

    Add Function That Builds Default Board Size

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const gridSize = document.querySelector('#grid-size');
 const gridSizeSpan = gridSize.querySelector('span');
 
-console.log(gridSizeSpan);
-
+//user can enter a custom pixel density for the board
 function createBoard(size) {
     let board = document.querySelector('.board');
+    gridSizeSpan.textContent = `${size} x ${size}`;
 
-    for (let i = 0; i < Math.pow(size, 2); i++) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        board.appendChild(pixel);
-    }    
-    return board;
+    if (size > 0 && size <= 100) {
+        for (let i = 0; i < Math.pow(size, 2); i++) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            board.appendChild(pixel);
+        }
+        return board;
+    } else {
+        return;
+    }
 }
 
- console.log(createBoard(8));
+//Upon loading of webpage, the default board size will be 16x16
+function defaultBoard(){
+    let board = document.querySelector('.board');
+
+        for (let i = 0; i < Math.pow(16, 2); i++) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            board.appendChild(pixel);
+        }
+        return board;
+
+}
