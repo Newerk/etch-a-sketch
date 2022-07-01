28259383dfca2b69baa1f675b56fc7f1934d3cc8
newerk <juniusowens@yahoo.com>
2022-07-01 16:56:49 -0400

    Add Working Dark/Light Mode Toggle

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 defaultBoard();
+const num = document.getElementById('num');
+
+var counter = 0;
+num.innerHTML = counter;
+
+
+
+function toggleFunction() {
+   num.innerHTML = counter++;
+
+   if (num.innerHTML % 2 === 0) {
+    darkMode();
+
+   } else{
+    lightMode();
+
+   }
+
+}
+
 
 //eventlisteners for clickable buttons on page
 // let topButtons = document.querySelector('.top');
@@ -8,21 +28,32 @@ defaultBoard();
 //         document.body.setAttribute('style', 'background-color: yellow');
 //     }   
 // });
+function darkMode() {
+    document.body.setAttribute('style', 'background-color: #38404A;color: white');
+    document.querySelector('.header').setAttribute('style', 'background-color: #181921');
+    document.querySelector('#custom').setAttribute('style', 'background-color: #181921; color: white;');
+    document.querySelector('#grid-size').setAttribute('style', 'background-color: #181921; color: white;');
+    document.querySelector('#black').setAttribute('style', 'background-color: #181921; color: white;');
+    document.querySelector('#rgb').setAttribute('style', 'background-color: #181921; color: white;');
+    document.querySelector('.erase').setAttribute('style', 'background-color: #181921; color: white;');
+}
 
-let toggle = document.querySelector('.switch');
-toggle.addEventListener('click', function(e){
+function lightMode() {
+    document.body.setAttribute('style', 'background-color: #white ;color: blacks');
 
-    if (e.target !== e.currentTarget) {
-                document.body.setAttribute('style', 'background-color: #38404A;color: white');
-                document.querySelector('.header').setAttribute('style', 'background-color: #181921');
-                document.querySelector('#custom').setAttribute('style', 'background-color: #181921; color: white;');
-                document.querySelector('#grid-size').setAttribute('style', 'background-color: #181921; color: white;');
-                document.querySelector('#black').setAttribute('style', 'background-color: #181921; color: white;');
-                document.querySelector('#rgb').setAttribute('style', 'background-color: #181921; color: white;');
-                document.querySelector('.erase').setAttribute('style', 'background-color: #181921; color: white;');
+    document.querySelector('.header').setAttribute('style', 'background-color: #D9D9D9');
+    document.querySelector('#custom').setAttribute('style', 'background-color: white; color: black;');
+    document.querySelector('#grid-size').setAttribute('style', 'background-color: white; color: black;');
+    document.querySelector('#black').setAttribute('style', 'background-color: white; color: black;');
+    document.querySelector('#rgb').setAttribute('style', 'background-color: white; color: black;');
+    document.querySelector('.erase').setAttribute('style', 'background-color: white; color: black;');
 
-    }
-});
+}
+
+
+
+
+
 
 
 const gridSize = document.querySelector('#grid-size');
@@ -48,23 +79,23 @@ function createBoard(size) {
 
 
 //Upon loading of webpage, the default board size will be 16x16
-function defaultBoard(){
+function defaultBoard() {
     let board = document.querySelector('.board');
 
-        for (let i = 0; i < Math.pow(16, 2); i++) {
-            let pixel = document.createElement('div');
-            pixel.className = 'pixel';
-            buildPixel(pixel, 16);
-            board.append(pixel);
-        }
+    for (let i = 0; i < Math.pow(16, 2); i++) {
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        buildPixel(pixel, 16);
+        board.append(pixel);
+    }
 
-        return board;
+    return board;
 }
 
 
 //write a function that calculates and sets the size of each pixel. will set the style in this function
 function buildPixel(pixel, size) {
-    let pixelLength = (696/size)-2;
+    let pixelLength = (696 / size) - 2;
 
     return pixel.setAttribute('style', `width: ${pixelLength}px; height: ${pixelLength}px;`);
 }
